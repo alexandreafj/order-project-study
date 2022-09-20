0a6577540cbf959a82da415f2f1dfc1c096b1a22
Alexandre Ferrreira <lexanjos@icloud.com>
2022-09-20 19:21:19 +0100

    change item mapper class

--- order-item-api/src/item/class/item-map.ts
+++ order-item-api/src/item/class/item-map.ts
@@ -1,27 +1,49 @@
-import { Item } from "src/entitys/item.entity";
-import { Mapper } from '../../common/interfaces/Mapper';
+import { Item } from "../../entitys/item.entity";
 import { ItemDto } from "../dto/Item-dto";
 
-class ItemMap implements Mapper<Item> {
-    toDomain(raw: any): Item {
+export abstract class ItemMap {
+    public static toEntity(raw: ItemDto): Item {
         return {
-            id: raw?.id ?? null,
-            description: raw?.description ?? null,
-            discount: raw?.discount ?? null,
-            name: raw?.name ?? null,
-            price: raw?.price ?? null,
-            type: raw?.type ?? null,
-        } as Item;
+            id: raw?.id ?? undefined,
+            description: raw?.description ?? undefined,
+            discount: raw?.discount ?? undefined,
+            name: raw?.name ?? undefined,
+            price: raw?.price ?? undefined,
+            type: raw?.type ?? undefined,
+        } as unknown as Item;
     }
-    toDTO(item: Item): ItemDto {
+    public static toEntityArray(raw: Array<ItemDto>): Array<Item> {
+        return raw?.map(entity => {
+            return {
+                id: entity?.id ?? undefined,
+                description: entity?.description ?? undefined,
+                discount: entity?.discount ?? undefined,
+                name: entity?.name ?? undefined,
+                price: entity?.price ?? undefined,
+                type: entity?.type ?? undefined,
+            } as unknown as Item;
+        }) ?? [];
+    }
+    public static toDTO(item: Item): ItemDto {
         return {
-            id: item?.id ?? null,
-            name: item?.name ?? null,
-            price: item?.price ?? null,
-            description: item?.description ?? null,
-            type: item?.type ?? null,
-            discount: item?.discount ?? null,
+            id: item?.id ?? undefined,
+            name: item?.name ?? undefined,
+            price: item?.price ?? undefined,
+            description: item?.description ?? undefined,
+            type: item?.type ?? undefined,
+            discount: item?.discount ?? undefined,
         } as unknown as ItemDto;
     }
-
+    public static toArrayDTO(arrItem: Array<Item>): Array<ItemDto> {
+        return arrItem?.map(entity => {
+            return {
+                id: entity?.id ?? undefined,
+                name: entity?.name ?? undefined,
+                price: entity?.price ?? undefined,
+                description: entity?.description ?? undefined,
+                type: entity?.type ?? undefined,
+                discount: entity?.discount ?? undefined,
+            } as unknown as ItemDto;
+        }) ?? []
+    }
 }
