a56eba1f74d41245bd8be0c240a6aca41cffaa49
Alexandre Ferrreira <lexanjos@icloud.com>
2022-09-20 19:21:49 +0100

    add class validators

--- order-item-api/src/item/class/item-filters.ts
+++ order-item-api/src/item/class/item-filters.ts
@@ -1,10 +1,22 @@
 import { ItemTypes } from "./item-types"
+import { IsNotEmpty, Length, IsEnum, IsOptional } from 'class-validator';
 
 class ItemFilters {
-    limit: number;
-    offset: number;
-    name: string;
-    price: number;
-    type: ItemTypes;
+    @IsNotEmpty()
+    limit = 10;
+
+    @IsNotEmpty()
+    offset = 0;
+
+    @Length(0, 255)
+    @IsOptional()
+    name?: string;
+
+    @IsOptional()
+    price?: number;
+
+    @IsEnum(ItemTypes)
+    @IsOptional()
+    type?: ItemTypes;
 }
 export { ItemFilters };
