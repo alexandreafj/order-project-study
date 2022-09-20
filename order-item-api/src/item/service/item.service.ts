1bfe8fb4234040241bde438376a60885e62a1095
Alexandre Ferrreira <lexanjos@icloud.com>
2022-09-20 19:20:20 +0100

    add functionality to service

--- order-item-api/src/item/service/item.service.ts
+++ order-item-api/src/item/service/item.service.ts
@@ -2,24 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { Item } from '../class/Item';
 import { ItemFilters } from '../class/item-filters';
 import { ItemDto } from '../dto/Item-dto';
-
+import { ItemRepository } from '../../repository/item.repository';
+import { ItemMap } from '../class/item-map';
+import { ItemDeleteDto } from '../dto/item-delete-dto';
 
 @Injectable()
 export class ItemService {
-    constructor() { }
+    constructor(private readonly itemRepository: ItemRepository) { }
 
-    async selectItems(filters: ItemFilters): Promise<Array<Item>> {
-        return [new Item()];
+    async selectItems(filters: ItemFilters): Promise<Array<ItemDto>> {
+        const itemsEntity = await this.itemRepository.select(filters);
+        const hasFoundAny = !!itemsEntity && itemsEntity?.length > 0;
+        if (hasFoundAny === false) return [];
+        const itemsDto = ItemMap.toArrayDTO(itemsEntity);
+        return itemsDto;
     }
 
     async insertItem(createItemDto: ItemDto) {
+        const itemEntity = ItemMap.toEntity(createItemDto);
+        delete itemEntity.id;
+        await this.itemRepository.save(itemEntity);
     }
 
-    async deleteItem(deleteItemsDto: Array<ItemDto>) {
-
+    async deleteItem(deleteItemsDto: ItemDeleteDto) {
+        await this.itemRepository.delete(deleteItemsDto);
     }
 
     async updateItem(updateItemDto: ItemDto) {
-
+        const updateItem = ItemMap.toEntity(updateItemDto);
+        await this.itemRepository.update(updateItem);
     }
 }
