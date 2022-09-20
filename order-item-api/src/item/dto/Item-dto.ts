7651edc4f3836da8992f820dbe5d91ae760ef95e
Alexandre Ferrreira <lexanjos@icloud.com>
2022-09-20 19:20:50 +0100

    add class validators so we can sanitize data

--- order-item-api/src/item/dto/Item-dto.ts
+++ order-item-api/src/item/dto/Item-dto.ts
@@ -1,10 +1,24 @@
 import { ItemTypes } from "../class/item-types";
+import { Length, IsEnum, IsOptional } from 'class-validator';
 
 export class ItemDto {
+    @IsOptional()
     id?: number;
+
+    @IsOptional()
+    @Length(0, 255)
     name?: string;
+
+    @IsOptional()
     price?: number;
+
+    @IsOptional()
     description?: string;
+
+    @IsOptional()
+    @IsEnum(ItemTypes)
     type?: ItemTypes;
+
+    @IsOptional()
     discount?: number;
 }
